22305637fe4cfb30d27b7483f83160eec1776029
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-01-24 11:01:37 +0100

    Last commit (I stg i'll kill myself, it's so good to end that)

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if ($interCount == 0) {
     $stmt->execute();
     $query = true;
 } elseif ($interCount > 0) {
-    $errors .= "L'intervention " . $inter . " possède déjà un questionnaire de rempli.";
+    $errors .= "L'intervention " . $inter . " possède déjà un questionnaire.";
     $query = false;
 }
 ?>
@@ -76,30 +76,101 @@ if ($interCount == 0) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Satisfaction Survey V2</title>
-    <link rel="stylesheet" href="./style.css">
+    <title>Satisfaction Survey V2</title><link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet"> 
+    <style>
+        * {
+            margin: 0;
+            padding : 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+        .container {
+            width: 100%;
+            height: 100vh;
+            background: #3c5077;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .popup {
+            width: 400px;
+            background: #fff;
+            border-radius: 6px;
+            position: absolute;
+            text-align: center;
+            padding: 0 30px 30px;
+            color: #333;
+            animation: open 0.5s ease;
+        }
+        @keyframes open {
+            0% {
+                top: 0;
+                left: 50%;
+                transform: translate(-50%, -50%) scale(0.1);
+                visibility: hidden;
+            }
+            100% {
+                visibility: visible;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%) scale(1);
+            }
+
+        }
+        .popup img {
+            width: 100px;
+            margin-top: -50%;
+            border-radius: 50%;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        }
+        .popup h1 {
+            font-size: 38px;
+            font-weight: 500;
+            margin: 30px 0 10px;
+            padding-bottom: 10px;
+        }
+        .popup button {
+            width: 100%;
+            margin-top: 35px;
+            padding: 10px 0;
+            background: #6fd649;
+            color: #fff;
+            border: 0;
+            outline: none;
+            font-size: 18px;
+            border-radius: 4px;
+            cursor: pointer;
+            box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
+        }
+    </style>
 </head>
 <body>
     <?php
     if ($query == true) { ?>
     <div class="container">
-        <div class="block" style="text-align:center">
-            <h1>Merci d'avoir donné votre avis !</h1>
-            <h2>Votre retour concernant l'intervention <?php echo $inter; ?> a bien été pris en compte</h2>
+        <div class="popup">
+            <img src="./tick.png">
+            <h1>Merci d'avoir donné votre avis</h1>
+            <p>Votre retour concernant l'intervention <?php echo $inter; ?> a bien été pris en compte</p>
             <p><span id="timer"></span></p>
+            <a href="https://www.officecenter.fr"><button type="button">OK</button></a>
         </div>
     </div>
     <?php } else { ?>
     <div class="container">
-        <div class="block">
-            <h1>Une erreur est parvenu pendant l'envoi de votre avis</h1>
-            <h2><?php echo $errors; ?></h2>
+        <div class="popup open-popup">
+            <img src="./no.png">
+            <h1>Une erreur est survenue :</h1>
+            <p><?php echo $errors; ?></p>
+            <p><span id="timer"></span></p>
+            <a href="https://www.officecenter.fr"><button type="button">OK</button></a>
         </div>
     </div>
     <?php }
     ?>
     
-    <!--
     <script type="text/javascript">
         let count = 10;
         let redirect = "https://www.officecenter.fr/";
@@ -113,7 +184,7 @@ if ($interCount == 0) {
                 window.location.href = redirect;
             }
         }
-        countdown();
+        //countdown();
     </script>-->
 </body>
 </html>
