4f9fea7683c1a309ab69f10a71914969b8f9c155
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-01-24 16:33:16 +0100

    new column added in db + url params checked

--- script.sql
+++ script.sql
@@ -1,10 +1,11 @@
 CREATE TABLE client_satisfaction (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    inter INT(10) NOT NULL,
+    inter INT(10) UNSIGNED NOT NULL,
     tech VARCHAR(50) NOT NULL,
     choice TINYINT UNSIGNED NOT NULL,
     survey_date VARCHAR(10) NOT NULL,
-    inter_date VARCHAR(10) NOT NULL
+    inter_date VARCHAR(10) NOT NULL,
+    email VARCHAR(100) NOT NULL
 )
 
-INSERT INTO client_satisfaction (inter, tech, choice, survey_date, inter_date) VALUES (123456, "goutorbe", 5, "23/01/2023", "20/01/2023")
+INSERT INTO client_satisfaction (inter, tech, choice, survey_date, inter_date) VALUES (123456, "goutorbe", 5, "23/01/2023", "20/01/2023", "johan@officecenter.fr")
