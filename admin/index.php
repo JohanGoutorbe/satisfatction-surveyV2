1ede18c0c7b80d7661d361c3e5030c9515307f3a
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-02-06 09:32:58 +0100

    icon added

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="../favicon.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Satisfaction Survey - Login Page - LOGIN</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
