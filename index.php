ebd663826cb1cb23b45e292192dbda8bbaa5588c
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-02-06 11:53:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -42,10 +42,15 @@ $url .= $_SERVER['REQUEST_URI'];
 $components = parse_url($url);
 parse_str($components['query'], $results);
 $inter = htmlspecialchars($results['inter']);
+$_SESSION['inter'] = $inter;
 $tech = strtolower(htmlspecialchars($results['tech']));
+$_SESSION['tech'] = $tech;
 $interdt = htmlspecialchars($results['date']);
+$_SESSION['interdt'] = $interdt;
 $choice = htmlspecialchars($results['choix']);
+$_SESSION['choice'] = $choice;
 $email = strtolower(htmlspecialchars($results['mail']));
+$_SESSION['email'] = $email;
 
 // Vérification du respect de la charte des paramètres de l'url
 if (ctype_digit($inter)) {
@@ -56,22 +61,22 @@ if (ctype_digit($inter)) {
                     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
                         $request = true;
                     } else {
-                        $errors .= "L'adresse email <strong>".$email."</strong> est incorrecte";
+                        $errors .= "L'adresse email <strong>" . $email . "</strong> est incorrecte";
                     }
                 } else {
-                    $errors .= "La notation sélectionnée <strong>".$choice."</strong> est incorrecte";
+                    $errors .= "La notation sélectionnée <strong>" . $choice . "</strong> est incorrecte";
                 }
             } else {
-                $errors .= "La date saisie <strong>".$interdt."</strong> est incorrecte";
+                $errors .= "La date saisie <strong>" . $interdt . "</strong> est incorrecte";
             }
         } else {
-            $errors .= "Le nom du technicien <strong>".$tech."</strong> est incorrect";
+            $errors .= "Le nom du technicien <strong>" . $tech . "</strong> est incorrect";
         }
     } else {
         $errors .= "Le numéro de l'intervention <strong>" . $inter . "</strong> est incorrect";
     }
 } else {
-    $errors .= "Le numéro de l'intervention <strong>".$inter."</strong> est incorrect";
+    $errors .= "Le numéro de l'intervention <strong>" . $inter . "</strong> est incorrect";
 }
 
 if ($request) {
@@ -100,7 +105,7 @@ if ($request) {
         $stmt->bindParam('interdt', $interdt);
         $stmt->bindParam('email', $email);
         $stmt->execute();
-        
+
         // Si l'email n'a pas encore mis 5 étoiles, rediriger vers la note Google
         if ($emailCount == 0 && $choice == 5) {
             header("Location: https://g.page/r/CQ_CHW3pUmqBEAI/review");
@@ -111,7 +116,7 @@ if ($request) {
         $getEmail = $db->prepare("SELECT * FROM `client_satisfaction` where email = ? and choice = 5");
         $getEmail->execute([$email]);
         $emailCount = $getEmail->rowCount();
-        
+
         // Application de la requête préparée
         $update = "UPDATE `client_satisfaction` SET survey_date = :dt, choice = :choice WHERE inter = :inter";
         $stmt = $db->prepare($update);
@@ -120,7 +125,7 @@ if ($request) {
         $stmt->bindParam('inter', $inter);
         $stmt->execute();
         $query = 2;
-        
+
         // Si l'email n'a pas encore mis 5 étoiles, rediriger vers la note Google
         if ($emailCount == 0 && $choice == 5) {
             header("Location: https://g.page/r/CQ_CHW3pUmqBEAI/review");
@@ -131,21 +136,24 @@ if ($request) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="./favicon.png" type="image/x-icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Satisfaction Survey V2</title><link rel="preconnect" href="https://fonts.googleapis.com">
+    <title>Satisfaction Survey V2</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet">
     <style>
         * {
             margin: 0;
-            padding : 0;
+            padding: 0;
             box-sizing: border-box;
             font-family: 'Poppins', sans-serif;
         }
+
         .container {
             width: 100%;
             height: 100vh;
@@ -155,6 +163,7 @@ if ($request) {
             justify-content: center;
             overflow: hidden;
         }
+
         .popup {
             width: 400px;
             background: #fff;
@@ -165,6 +174,7 @@ if ($request) {
             color: #333;
             animation: open 0.75s ease;
         }
+
         @keyframes open {
             0% {
                 top: 0;
@@ -172,6 +182,7 @@ if ($request) {
                 transform: translate(-50%, -50%) scale(0.1);
                 visibility: hidden;
             }
+
             100% {
                 visibility: visible;
                 left: 50%;
@@ -179,18 +190,21 @@ if ($request) {
                 transform: translate(-50%, -50%) scale(1);
             }
         }
+
         .popup img {
             width: 100px;
             margin-top: -50%;
             border-radius: 50%;
             box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
         }
+
         .popup h1 {
             font-size: 38px;
             font-weight: 500;
             margin: 30px 0 10px;
             padding-bottom: 10px;
         }
+
         .popup button {
             width: 100%;
             margin-top: 35px;
@@ -206,53 +220,56 @@ if ($request) {
         }
     </style>
 </head>
+
 <body>
     <?php
     if ($query == 1) { ?>
-    <div class="container">
-        <div class="popup">
-            <img src="./tick.png">
-            <h1>Merci d'avoir donné votre avis</h1>
-            <p>Votre note de <?php echo $choice ?> étoiles concernant l'intervention <?php echo $inter; ?> a bien été pris en compte</p>
-            <p><span id="timer"></span></p>
-            <a href="https://www.officecenter.fr"><button type="button">OK</button></a>
+        <div class="container">
+            <div class="popup">
+                <img src="./tick.png">
+                <h1>Merci d'avoir donné votre avis</h1>
+                <p>Votre note de <?php echo $choice ?> étoiles concernant l'intervention <?php echo $inter; ?> a bien été pris en compte</p>
+                <p><span id="timer"></span></p>
+                <a href="https://www.officecenter.fr"><button type="button">OK</button></a>
+            </div>
         </div>
-    </div>
     <?php } elseif ($query == 0) { ?>
-    <div class="container">
-        <div class="popup open-popup">
-            <img src="./no.png">
-            <h1>Une erreur est survenue :</h1>
-            <p><?php echo $errors; ?></p>
-            <p><span id="timer"></span></p>
-            <a href="https://www.officecenter.fr"><button type="button">OK</button></a>
+        <div class="container">
+            <div class="popup open-popup">
+                <img src="./no.png">
+                <h1>Une erreur est survenue :</h1>
+                <p><?php echo $errors; ?></p>
+                <p><span id="timer"></span></p>
+                <a href="https://www.officecenter.fr"><button type="button">OK</button></a>
+            </div>
         </div>
-    </div>
     <?php } elseif ($query == 2) { ?>
         <div class="container">
-        <div class="popup open-popup">
-            <img src="./tick.png">
-            <h1>Votre choix a bien été modifié</h1>
-            <p>Votre nouvelle note de <?php echo $choice ?> étoiles concernant l'intervention <?php echo $inter; ?> a bien été pris en compte</p>
-            <p><span id="timer"></span></p>
-            <a href="https://www.officecenter.fr"><button type="button">OK</button></a>
-        </div>
-    <?php } ?>
-    
-    <script type="text/javascript">
-        let count = 5;
-        let redirect = "https://www.officecenter.fr/";
-        function countdown() {
-            let timer = document.getElementById("timer");
-            if (count > 0) {
-                count--;
-                timer.innerHTML = "<br>Cette page sera redirigée dans <br><strong>"+count+" secondes.</strong>";
-                setTimeout("countdown()", 1000);
-            } else {
-                window.location.href = redirect;
+            <div class="popup open-popup">
+                <img src="./tick.png">
+                <h1>Votre choix a bien été modifié</h1>
+                <p>Votre nouvelle note de <?php echo $choice ?> étoiles concernant l'intervention <?php echo $inter; ?> a bien été pris en compte</p>
+                <p><span id="timer"></span></p>
+                <a href="http://localhost/satisfaction-surveyV2/ajouter_un_commentaire"><button type="button">OK</button></a>
+            </div>
+        <?php } ?>
+
+        <script type="text/javascript">
+            let count = 5;
+            let redirect = "https://www.officecenter.fr";
+
+            function countdown() {
+                let timer = document.getElementById("timer");
+                if (count > 0) {
+                    count--;
+                    timer.innerHTML = "<br>Cette page sera redirigée dans <br><strong>" + count + " secondes.</strong>";
+                    setTimeout("countdown()", 1000);
+                } else {
+                    window.location.href = redirect;
+                }
             }
-        }
-        countdown();
-    </script>-->
+            //countdown();
+        </script>-->
 </body>
+
 </html>
